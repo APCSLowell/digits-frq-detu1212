4fc140aeaffd5693e4453772961159f9e35a219c
detu1212 <143124200+detu1212@users.noreply.github.com>
2024-04-22 12:55:24 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -24,9 +24,9 @@ public boolean isStrictlyIncreasing()
 for(int i = 1; i < digitList.size(); i++){
 if(digitList.get(i) > digitList.get(i-1)){
 return true;
-// for(int i = 0; i < didgitList.size()-1; i++){
-//  if(digitList.get(i+1) <= digitList.get(i)){
-//  return false;
+for(int i = 0; i < didgitList.size()-1; i++){
+if(digitList.get(i+1) <= digitList.get(i)){
+return false;
 }
 }
 return false;
