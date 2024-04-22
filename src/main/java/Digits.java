90cbfb41b866d02fdaba9e076a1bb21d980ba47e
detu1212 <143124200+detu1212@users.noreply.github.com>
2024-04-22 15:01:56 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -8,18 +8,31 @@ private ArrayList<Integer> digitList;
 public Digits(int num)
 { 
 digitList = new ArrayList <Integer>();
-String str = "" + num;
-    for (int i = 0; i < str.length(); i++)
-        digitList.add(str.charAt(i) - '0');
+if(num == 0){
+digitList.add(0);
+}
+int n = num;
+while(num > 0 ){
+digitList.add(0, n%10);
+n = n%10;
 }
     
 }
 
 public boolean isStrictlyIncreasing()
 { 
-for(int i = 0; i < digitList.size()-1; i++){
-if(digitList.get(i) <=digitList.get(i))
-return false; 
-    return true;
+for(int i = 1; i < digitList.size(); i++){
+if(digitList.get(i) > digitList.get(i-1)){
+return true;
+// for(int i = 0; i < didgitList.size()-1; i++){
+//  if(digitList.get(i+1) <= digitList.get(i)){
+//  return false;
+}
+}
+return false;
+}
+public String toString()
+{
+return digitList.toString();
 }
 }
