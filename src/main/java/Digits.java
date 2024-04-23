026b195b1a67497f19d6d5e25bab69a35e3bebbb
detu1212 <143124200+detu1212@users.noreply.github.com>
2024-04-22 21:21:27 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -3,36 +3,31 @@ import java.util.ArrayList;
 public class Digits
 {
 
-private ArrayList<Integer> digitList;
+	private ArrayList<Integer> digitList;
 
-public Digits(int num)
-{ 
-digitList = new ArrayList <Integer>();
-if(num == 0){
-digitList.add(0);
-}
-int n = num;
-while(num > 0 ){
-digitList.add(0, n%10);
-n = n%10;
-}
-    
-}
+	public Digits(int num)
+	{ /* to be implemented in part (a) */ 
+	    digitList = new ArrayList <Integer>();
+		if (num == 0)
+			digitList.add(0);
+		int n = num;
+		while (n > 0){
+			digitList.add(0,n%10);
+			n=n/10;
+		}
+	}
 
-public boolean isStrictlyIncreasing()
-{ 
-for(int i = 1; i < digitList.size(); i++){
-if(digitList.get(i) > digitList.get(i-1)){
-return true;
-// for(int i = 0; i < didgitList.size()-1; i++){
-//  if(digitList.get(i+1) <= digitList.get(i)){
-//  return false;
-}
-}
-return false;
-}
-public String toString()
-{
-return digitList.toString();
-}
+	public boolean isStrictlyIncreasing()
+	{ /* to be implemented in part (b) */
+		for (int i = 0; i < digitList.size()-1;i++)
+			if(digitList.get(i+1)<=digitList.get(i))
+				return false;
+		return true;
+
+	}
+	
+	public String toString()
+	{
+		return digitList.toString();
+	}
 }
